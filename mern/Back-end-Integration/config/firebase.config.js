2d97a76b7731e9ad7593b8688824afd6bf1f675c
suhel <thecomputersk@gamil.com>
2025-03-27 15:04:07 +0530

    Fixed Back-end-Integration submodule issue and re-added as normal folder

---
+++ mern/Back-end-Integration/config/firebase.config.js
@@ -0,0 +1,10 @@
+const fbAdmin = require('firebase-admin');
+
+const serviceCredentials = require('../etc/secrete/durable-stack-449615-n0-firebase-adminsdk-fbsvc-6377ce54f4.json');
+
+
+fbAdmin.initializeApp({ credential: fbAdmin.credential.cert(serviceCredentials),
+    storageBucket: "gs://durable-stack-449615-n0.firebasestorage.app"
+ });
+
+module.exports = fbAdmin;
